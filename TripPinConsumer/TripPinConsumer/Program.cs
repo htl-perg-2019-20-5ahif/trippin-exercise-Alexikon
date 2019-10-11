ebfb03922df1f875a291e504e44d1b0d1ce360da
Alexikon <alex.tischlinger@gmail.com>
2019-10-11 19:07:37 +0200

    started Console Program

---
+++ TripPinConsumer/TripPinConsumer/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace TripPinConsumer
+{
+    class Program
+    {
+        private static HttpClient HttpClient = new HttpClient()
+        {
+            BaseAddress = new Uri("http://services.odata.org/TripPinRESTierService/(S(3mslpb2bc0k5ufk24olpghzx))/")
+        };
+
+        static async Task Main(string[] args)
+        {
+            // JSON Deserializer kann objekt nicht ´deserializen
+            var jsonFileText = await File.ReadAllTextAsync("users.json");
+            var users = JsonSerializer.Deserialize<Users>(jsonFileText);
+
+
+
+
+
+            //foreach (var user in users.Results)
+            //{
+
+            //}
+
+
+
+            // Test, ob HttpClient Funktioniert
+            var tripPinResponse = await HttpClient.GetAsync("People(\'russellwhyte\')");
+            var dfjh = tripPinResponse.StatusCode;
+            var responseBody = await tripPinResponse.Content.ReadAsStringAsync();
+            Console.WriteLine(dfjh + "\n\n" + responseBody);
+
+
+
+
+        }
+    }
+}
