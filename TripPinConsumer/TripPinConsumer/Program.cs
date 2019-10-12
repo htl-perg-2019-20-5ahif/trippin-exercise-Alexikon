ad4fde92c8164dec3b31328e5801057fdb781307
Alexikon <alex.tischlinger@gmail.com>
2019-10-12 07:16:13 +0200

    added comments

--- TripPinConsumer/TripPinConsumer/Program.cs
+++ TripPinConsumer/TripPinConsumer/Program.cs
@@ -18,14 +18,17 @@ namespace TripPinConsumer
 
         static async Task Main(string[] args)
         {
-            // JSON Deserializer kann objekt nicht ´deserializen
+            // read users.json and make that into an object
             var jsonFileText = await File.ReadAllTextAsync("users.json");
             var users = JsonSerializer.Deserialize<List<JsonUser>>(jsonFileText);
 
+            // iterate through users
             foreach (var user in users)
             {
+                // get the user with specific username from Sercice
                 var tripPinResponse = await HttpClient.GetAsync("People(\'" + user.UserName + "\')");
 
+                // if user does not exist, create that user
                 if(!tripPinResponse.IsSuccessStatusCode)
                 {
                     var city = new City();
@@ -45,8 +48,11 @@ namespace TripPinConsumer
                     userService.AddressInfo = new List<AddressInfo>();
                     userService.AddressInfo.Add(addressInfo);
 
+                    // post the user to Service
                     var content = new StringContent(JsonSerializer.Serialize(userService), Encoding.UTF8, "application/json");
                     var postResponse = await HttpClient.PostAsync("People", content);
+
+                    // watch if user is successfully created
                     try
                     {
                         postResponse.EnsureSuccessStatusCode();
@@ -59,6 +65,7 @@ namespace TripPinConsumer
                     }
 
                 }
+                // user already exists
                 else
                 {
                     Console.WriteLine("Person " + user.UserName + " already in TripPinRestService");
